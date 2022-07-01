9ce4646da98c882b603ac5bdcebc45e7c8cfb245
Happy_Ta <will231897@gmail.com>
2022-07-01 08:53:51 -0400

    Created android studio workspace
    Initial creation of the android studio workspace

---
+++ app/src/main/java/edu/wit/mobileapp/c_4_u_me_mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.wit.mobileapp.c_4_u_me_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
