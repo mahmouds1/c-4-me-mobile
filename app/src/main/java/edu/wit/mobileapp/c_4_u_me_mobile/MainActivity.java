d58aed7c8db9c2cbd0fd8176ff7305f6d56ccf39
Sarah <mahmouds@wit.edu>
2022-07-12 13:02:12 -0400

    added logs for testing and application lifecycle methods to main activity

--- app/src/main/java/edu/wit/mobileapp/c_4_u_me_mobile/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/c_4_u_me_mobile/MainActivity.java
@@ -3,12 +3,62 @@ package edu.wit.mobileapp.c_4_u_me_mobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    static String TAG = "myApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "onCreate() is called");
+
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.v(TAG, "onStart() is called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.v(TAG, "onResume() is called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.v(TAG, "onPause() is called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.v(TAG, "onStop() is called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.v(TAG, "onRestart() is called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.v(TAG, "onDestroy() is called");
+    }
+
 }
