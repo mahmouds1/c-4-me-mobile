43cdde865d8256c1a1f1681710ebacba3827ca43
Happy_Ta <will231897@gmail.com>
2022-07-15 08:27:41 -0400

    Simple Map implemented

--- app/src/main/java/edu/wit/mobileapp/c_4_u_me_mobile/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/c_4_u_me_mobile/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.wit.mobileapp.c_4_u_me_mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +18,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.v(TAG, "onCreate() is called");
+
+        Button mapBtn = (Button) findViewById(R.id.mapButton);
+
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create intent and bundle for MapsActivity
+                Intent mapIntent = new Intent(MainActivity.this, MapsActivity.class );
+                Bundle mapBundle = new Bundle();
+
+                mapIntent.putExtras(mapBundle);
+
+                // Send to map
+                startActivity(mapIntent);
+            }
+        });
 
     }
 
