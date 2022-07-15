6afe8f4f19660276f8666cfe55c2eaaaca9bcb60
Sarah <mahmouds@wit.edu>
2022-07-15 11:27:45 -0400

    adding activity redirects from main

--- app/src/main/java/edu/wit/mobileapp/c_4_u_me_mobile/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/c_4_u_me_mobile/MainActivity.java
@@ -2,8 +2,11 @@ package edu.wit.mobileapp.c_4_u_me_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +15,76 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //linking java with layout
 
         Log.v(TAG, "onCreate() is called");
 
-    }
+
+        //buttons
+        Button notesBtn = (Button) findViewById(R.id.notesSwitchButton);
+        Button mapsBtn = (Button) findViewById(R.id.mapsSwitchButton);
+        Button connectivityBtn = (Button) findViewById(R.id.connectivitySwitchButton);
+        Button settingsBtn = (Button) findViewById(R.id.settingsSwitchButton);
+
+
+        //notes activity switch button listener
+        mapsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "notes button clicked");
+
+                //intent for activity switch
+                Intent notesIntent = new Intent();
+                //notesIntent.setClass(MainActivity.this, NotesActivity.class);
+
+                startActivity(notesIntent);
+            }
+        });//end of mapsBtn listener
+
+        //maps activity switch button listener
+        mapsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "maps button clicked");
+
+                //intent for activity switch
+                Intent mapsIntent = new Intent();
+                //mapsIntent.setClass(MainActivity.this, MapsActivity.class);
+
+                startActivity(mapsIntent);
+            }
+        });//end of mapsBtn listener
+
+
+        //connectivity activity switch button listener
+        mapsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "connectivity button clicked");
+
+                //intent for activity switch
+                Intent connectivityIntent = new Intent();
+                //connectivityIntent.setClass(MainActivity.this, ConnectivityActivity.class);
+
+                startActivity(connectivityIntent);
+            }
+        });//end of mapsBtn listener
+
+
+        //settings activity switch button listener
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "settings button clicked");
+
+                //intent for activity switch
+                Intent settingsIntent = new Intent();
+                settingsIntent.setClass(MainActivity.this, SettingsActivity.class);
+
+                startActivity(settingsIntent);
+            }
+        });//end of settingsBtn listener
+    }//end of onCreate()
 
 
     @Override
