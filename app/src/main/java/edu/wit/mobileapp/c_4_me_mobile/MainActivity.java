4c9afe1cdc9ba09a7dac9d57384fefdb336b41ef
Owen Moreau <moreauowen@gmail.com>
2022-07-21 11:34:00 -0400

    Code styling changes

--- app/src/main/java/edu/wit/mobileapp/c_4_me_mobile/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/c_4_me_mobile/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+
 public class MainActivity extends AppCompatActivity {
 
     static String TAG = "myApp";
@@ -13,51 +14,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Log.v(TAG, "onCreate() is called");
     }
-
 
     @Override
     protected void onStart() {
         super.onStart();
-
         Log.v(TAG, "onStart() is called");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         Log.v(TAG, "onResume() is called");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
         Log.v(TAG, "onPause() is called");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-
         Log.v(TAG, "onStop() is called");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-
         Log.v(TAG, "onRestart() is called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
         Log.v(TAG, "onDestroy() is called");
     }
-
 }
