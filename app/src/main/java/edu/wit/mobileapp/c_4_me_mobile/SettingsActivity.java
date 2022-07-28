1a12d12a990d989b55626a848da85002afe0ce2e
Sarah <mahmouds@wit.edu>
2022-07-28 14:32:19 -0400

    renamed xml file

--- app/src/main/java/edu/wit/mobileapp/c_4_me_mobile/SettingsActivity.java
+++ app/src/main/java/edu/wit/mobileapp/c_4_me_mobile/SettingsActivity.java
@@ -1,9 +1,7 @@
 package edu.wit.mobileapp.c_4_me_mobile;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,15 +13,15 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.activity_settings);
 
         Log.v(TAG, "onCreate() is called");
 
 
         Button homeBtn = (Button) findViewById(R.id.settingsSwitchButton);
 
-        //do we need this??
-        /*
+
+        /* //do we need this??
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
